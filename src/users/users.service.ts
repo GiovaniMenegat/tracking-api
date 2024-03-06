84a1f3b4054d2f52f0462fcc06602dbb71fee57d
GiovaniMenegat <gi.menegat10@gmail.com>
2024-03-06 09:06:58 -0300

    feat: check if user exists in findOne

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './repositories/users.repository';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 
 @Injectable()
 export class UsersService {
@@ -15,8 +16,14 @@ export class UsersService {
     return this.usersRepository.findAll();
   }
 
-  findOne(id: number) {
-    return this.usersRepository.findOne(id);
+  async findOne(id: number) {
+    const user = await this.usersRepository.findOne(id);
+
+    if (!user) {
+      throw new NotFoundError('Usuário não encontrado.');
+    }
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
