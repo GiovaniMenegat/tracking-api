d281c6918878dab3be1b9d6b5177e29a07ba91f6
GiovaniMenegat <gi.menegat10@gmail.com>
2024-03-06 08:09:38 -0300

    feat: validation class

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  password: string;
+}
