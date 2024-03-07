df95cd43b0be826e07ef3c597bde54e8a17e0331
GiovaniMenegat <gi.menegat10@gmail.com>
2024-03-07 07:46:00 -0300

    feat: packages resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PackagesModule } from './packages/packages.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule],
+  imports: [ConfigModule.forRoot(), UsersModule, PackagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
