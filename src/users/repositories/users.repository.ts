535ebefcbd96db2e7888266745ced5d38c9cfe85
GiovaniMenegat <gi.menegat10@gmail.com>
2024-03-18 07:42:08 -0300

    feat: remove unused routes

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -7,20 +7,6 @@ import { UpdateUserDto } from '../dto/update-user.dto';
 export class UsersRepository {
   constructor(private readonly prisma: PrismaService) {}
 
-  // async create(createUserDto: CreateUserDto): Promise<UserEntity> {
-  //   return this.prisma.user.create({
-  //     data: createUserDto,
-  //   });
-  // }
-
-  async findAll(): Promise<UserEntity[]> {
-    return this.prisma.user.findMany();
-  }
-
-  async findOne(id: number): Promise<UserEntity> {
-    return this.prisma.user.findUnique({ where: { id } });
-  }
-
   async update(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
     return this.prisma.user.update({
       where: { id },
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,30 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './repositories/users.repository';
-import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly usersRepository: UsersRepository) {}
-
-  // create(createUserDto: CreateUserDto) {
-  //   return this.usersRepository.create(createUserDto);
-  // }
-
-  findAll() {
-    return this.usersRepository.findAll();
-  }
-
-  async findOne(id: number) {
-    const user = await this.usersRepository.findOne(id);
-
-    if (!user) {
-      throw new NotFoundError('User not found');
-    }
-
-    return user;
-  }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return this.usersRepository.update(id, updateUserDto);
