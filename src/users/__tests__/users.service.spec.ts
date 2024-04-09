630ad3fd083f632f25a324d0d4b22bd84685f9f6
GiovaniMenegat <gi.menegat10@gmail.com>
2024-04-09 18:52:41 -0300

    test: users service tests

--- src/users/__tests__/users.service.spec.ts
+++ src/users/__tests__/users.service.spec.ts
@@ -2,19 +2,61 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from '../users.service';
 import { UsersRepository } from '../repositories/users.repository';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaUserMock } from './user.service.mock';
+import { UserEntity } from '../entities/user.entity';
+import { CreateUserMock } from 'src/auth/__tests__/auth.service.mock';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService, UsersRepository, PrismaService],
+      providers: [UsersService, UsersRepository, PrismaService, PrismaUserMock],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(usersService).toBeDefined();
+  });
+
+  describe('Update service', () => {
+    it('should update a user', async () => {
+      const updateUserMock = {
+        username: 'newUsername',
+      } as Partial<UserEntity>;
+
+      prisma.user.update = jest.fn().mockReturnValue({
+        ...CreateUserMock,
+        username: updateUserMock.username,
+      });
+
+      const result = await usersService.update(1, updateUserMock);
+
+      expect(result).toEqual({
+        ...CreateUserMock,
+        username: updateUserMock.username,
+      });
+      expect(prisma.user.update).toHaveBeenCalled();
+      expect(prisma.user.update).toHaveBeenCalledWith({
+        where: { id: 1 },
+        data: updateUserMock,
+      });
+    });
+  });
+
+  describe('Delete service', () => {
+    it('should delete a user', async () => {
+      const result = await usersService.remove(1);
+
+      expect(result).toEqual(CreateUserMock);
+      expect(prisma.user.delete).toHaveBeenCalled();
+      expect(prisma.user.delete).toHaveBeenCalledWith({
+        where: { id: 1 },
+      });
+    });
   });
 });
