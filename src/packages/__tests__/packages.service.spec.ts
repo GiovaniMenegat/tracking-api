ee408e1b61059b0536ea396d865e8498cb4f3a68
GiovaniMenegat <gi.menegat10@gmail.com>
2024-04-10 09:03:27 -0300

    test: packages create service

--- src/packages/__tests__/packages.service.spec.ts
+++ src/packages/__tests__/packages.service.spec.ts
@@ -2,19 +2,67 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PackagesService } from '../packages.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PackagesRepository } from '../repositories/packages.repository';
+import {
+  PrismaPackagesMock,
+  createPackageDto,
+  packageEntityMock,
+} from './packages.service.mock';
 
 describe('PackagesService', () => {
-  let service: PackagesService;
+  let packagesService: PackagesService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PackagesService, PackagesRepository, PrismaService],
+      providers: [
+        PackagesService,
+        PackagesRepository,
+        PrismaService,
+        PrismaPackagesMock,
+      ],
     }).compile();
 
-    service = module.get<PackagesService>(PackagesService);
+    packagesService = module.get<PackagesService>(PackagesService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(packagesService).toBeDefined();
+  });
+
+  describe('Create service', () => {
+    it('should create a package', async () => {
+      const result = await packagesService.create(createPackageDto, 1);
+
+      expect(prisma.user.findUnique).toHaveBeenCalled();
+      expect(prisma.packages.create).toHaveBeenCalled();
+      expect(result).toEqual(packageEntityMock);
+      expect(prisma.user.findUnique).toHaveBeenCalledWith({
+        where: {
+          id: 1,
+        },
+      });
+      expect(prisma.packages.create).toHaveBeenCalledWith({
+        data: {
+          ...createPackageDto,
+          user: {
+            connect: {
+              id: 1,
+            },
+          },
+        },
+      });
+    });
+
+    it('should return User not found when user not found', async () => {
+      prisma.user.findUnique = jest.fn().mockResolvedValue(false);
+
+      try {
+        await packagesService.create(createPackageDto, 1);
+      } catch (error) {
+        expect(error.message).toEqual('User not found');
+        expect(error.status).toEqual(404);
+      }
+    });
   });
 });
